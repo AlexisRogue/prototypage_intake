e534dd0fac3563a3d75c6cddb45db0a9fac2dfee
Alexis <Rosa.alexis@cra.education>
2026-01-28 17:09:36 -0500

    Intake V2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -11,26 +10,26 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
-  Joystick stick = new Joystick(0);
-  JoystickButton button1 = new JoystickButton(stick, 0);
-  JoystickButton button2 = new JoystickButton(stick, 1);
-  JoystickButton button3 = new JoystickButton(stick, 2);
-  JoystickButton button4 = new JoystickButton(stick, 3);
-  Intake intake = new Intake();
 
-  public RobotContainer() {
-    configureBindings();
+    Joystick stick = new Joystick(0);
+    JoystickButton button1 = new JoystickButton(stick, 0);
+    JoystickButton button2 = new JoystickButton(stick, 1);
+    JoystickButton button3 = new JoystickButton(stick, 2);
+    Intake intake = new Intake();
 
-    button1.onTrue(intake.spinStopIntake(0.5));// Intake le fuel
-    button2.onTrue(intake.extendIntake(0.5)).onFalse(intake.stopExtending());// Extend le intake
-    button3.onTrue(intake.retractIntake(0.5)).onFalse(intake.stopExtending());// Retract le intake
-  }
+    public RobotContainer() {
+        configureBindings();
 
-  private void configureBindings() {
+        button1.onTrue(intake.spinStopIntake(0.5));// Intake le fuel
+        button2.onTrue(intake.extendIntake(0.5)).onFalse(intake.stopExtending());// Extend le intake
+        button3.onTrue(intake.retractIntake(0.5)).onFalse(intake.stopExtending());// Retract le intake
+    }
 
-  }
+    private void configureBindings() {
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// Copyright (c) FIRST and other WPILib contributors.// Open Source Software; you can modify and/or share it under the terms of// the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -14,59 +11,58 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  boolean spinning = false;
-  boolean extending = false;
-  TalonFX rollerMotor;
-  TalonFX extendLeftMotor;
-  TalonFX extendRightMotor;
-  TalonFXConfiguration leftMotorConfig;
-  TalonFXConfiguration rightMotorConfig;
 
-  /** Creates a new Intake. */
-  public Intake() {
-    rollerMotor = new TalonFX(Constants.kRollerMotorID);
-    extendLeftMotor = new TalonFX(Constants.kExtendLeftMotorID);
-    extendRightMotor = new TalonFX(Constants.kExtendRightMotorID);
-    leftMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    rightMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-  }
+    boolean spinning = false;
+    boolean extending = false;
+    TalonFX rollerMotor;
+    TalonFX extendMotor;
+    TalonFXConfiguration extendingMotorConfig;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    /**
+     * Creates a new Intake.
+     */
+    public Intake() {
+        rollerMotor = new TalonFX(Constants.kRollerMotorID);
+        extendMotor = new TalonFX(Constants.kExtendMotorID);
+        extendingMotorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    }
 
-  public Command spinIntake(double voltage) {
-    spinning = true;
-    return this.run(() -> rollerMotor.setVoltage(voltage));
-  }
+    @Override
 
-  public Command stopIntake() {
-    spinning = false;
-    return this.run(() -> rollerMotor.setVoltage(0));
-  }
+    public void periodic() {
+        // This method will be called once per scheduler run  }
+    }
 
-  public Command extendIntake(double voltage) {
-    extending = true;
-    return this.run(() -> spinIntakeMotors(voltage + Constants.kGExtension));
-  }
+    public Command spinIntake(double voltage) {
+        spinning = true;
+        return this.run(() -> rollerMotor.setVoltage(voltage));
+    }
 
-  public Command stopExtending() {
-    return this.run(() -> spinIntakeMotors(Constants.kGExtension));
-  }
+    public Command stopIntake() {
+        spinning = false;
+        return this.run(() -> rollerMotor.setVoltage(0));
+    }
 
-  public void spinIntakeMotors(double voltage) {
-    extendLeftMotor.setVoltage(voltage + Constants.kGExtension);
-    extendRightMotor.setVoltage(voltage + Constants.kGExtension);
-  }
+    public Command extendIntake(double voltage) {
+        extending = true;
+        return this.run(() -> extendMotor.setVoltage(voltage + Constants.kGExtension));
 
-  public Command retractIntake(double voltage) {
-    extending = false;
-    return this.run(() -> spinIntakeMotors(-voltage + Constants.kGExtension));
-  }
+    }
 
-  public ConditionalCommand spinStopIntake(double voltage) {
-    return new ConditionalCommand(stopIntake(), spinIntake(voltage), () -> spinning);
+    public Command stopExtending() {
+        extending = false;
+        return this.run(() -> extendMotor.setVoltage(Constants.kGExtension));
 
-  }
+    }
+
+    public Command retractIntake(double voltage) {
+        extending = false;
+        return this.run(() -> extendMotor.setVoltage(-voltage + Constants.kGExtension));
+
+    }
+
+    public ConditionalCommand spinStopIntake(double voltage) {
+        return new ConditionalCommand(stopIntake(), spinIntake(voltage), () -> spinning);
+
+    }
 }
